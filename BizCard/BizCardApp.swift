f92a37aa6f1b2a308820e7ed96b9e09ccefa10c3
Sherry1481 <shareyscorpi1@gmail.com>
2021-08-15 00:23:10 +0500

    Initial Commit

---
+++ BizCard/BizCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BizCardApp.swift
+//  BizCard
+//
+//  Created by sherry on 14/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct BizCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BizCard/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  BizCard
+//
+//  Created by sherry on 14/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            
+            Image("sherry1")
+                .resizable()
+            .scaledToFit()
+                .clipShape(Circle())
+                .overlay(Circle()
+                    .stroke(Color.white, lineWidth: 3))
+                .shadow(radius: 5)
+//                .frame(width: 180, height:180, alignment: .center)
+            
+            
+            VStack() {
+                Text("Shaharyar")
+                    .font(.title)
+                    .foregroundColor(.white)
+                
+                Text("buildappswithme.com")
+                    .font(.subheadline)
+                    .foregroundColor(.white)
+                HStack{
+                    Image(systemName: "t.square.fill")
+                        .foregroundColor(.pink)
+                    
+                    Text(": @buildappswithme")
+                        .font(.subheadline)
+                        .foregroundColor(Color.white)
+                        .bold()
+                        .italic()
+                    
+                }
+            }
+            
+        }.frame(width: 380, height: 200)
+        .background(Color.green)
+        .cornerRadius(8)
+        .shadow(radius: 5)
+        
+        
+}
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
